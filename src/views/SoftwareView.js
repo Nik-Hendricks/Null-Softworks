c077c63c52482deecb008c65964932ab194581b0
Nik Hendricks <nikhendricks43@Gmail.com>
2023-01-12 15:49:17 -0600

    more page grounwork

--- src/views/SoftwareView.js
+++ src/views/SoftwareView.js
@@ -8,29 +8,41 @@ class SoftwareView extends View{
     connectedCallback(){
         this.classList.add('view')
         var l = window.location.href.split('/').length -1
-        var detail_view = (l == 4) ? true: false;
-        var name = (detail_view) ? window.location.href.split('/')[l].split('%20').join(' '): undefined;
-        console.log(name)
+        this.detail_view = (l == 4) ? true: false;
+        this.name = (this.detail_view) ? window.location.href.split('/')[l].split('%20').join(' '): undefined;
 
-        this.software_button = window.Builder.createElement('custom-input', {type:'button', width:'12', text:'test', icon:'info'}, {})
-        this.append(this.software_button)
-
-        var games = [
+        this.games = [
             {name:'Top down shooter', images:[], description:"Top down shooter game. You get to shoot stuff and things. also you can walk!"},
-            {name:'Top down shooter', images:[], description:"Top down shooter game. You get to shoot stuff and things. also you can walk!"},
-            {name:'Top down shooter', images:[], description:"Top down shooter game. You get to shoot stuff and things. also you can walk!"},
-            {name:'Top down shooter', images:[], description:"Top down shooter game. You get to shoot stuff and things. also you can walk!"},
+            {name:'Other cool game', images:[], description:" Top down shooter game. You get to shoot stuff and things. also you can walk!"},
+            {name:'Other other cool game', images:[], description:" Top down shooter game. You get to shoot stuff and things. also you can walk!"},
+            {name:'Bottom up shooter!', images:[], description:"  Top down shooter game. You get to shoot stuff and things. also you can walk!"},
         ]
-
-        games.forEach(game => {
-            var el = window.Builder.createElement('card-item', {width:'6', square:true});
-            el.onclick = () => {
-                window.history.pushState('', '', `/Software/${game.name}`)
-            }
-            this.append(el)
-        })
+        this.append(this.createBody())
 
         window.DP.dispatch('VIEW_LOAD')
+    }
+
+    createBody(){
+        var d = document.createElement('div')
+        if(this.detail_view){
+            var el = window.Builder.createElement('card-item', {width:'12', square:true})
+            el.append(window.Builder.createElement('custom-text', {align:'center', color:'var(--theme-text-primary-color)', width: '12', text:this.name}))
+            d.append(el)
+        }else{
+            this.games.forEach(game => {
+                var el = window.Builder.createElement('card-item', {width:'6', square:true});
+                el.append(window.Builder.createElement('custom-text', {align:'center', color:'var(--theme-text-primary-color)', width: '12', text:game.name}))
+                el.onclick = () => {
+                    window.history.pushState('', '', `/Software/${game.name}`)
+                }
+                d.append(el)
+            })
+            
+        }
+
+        
+        return d
+       
     }
 }
 
